4f3a8f9efc03df7c9189b17d2fe6cfe6dd666006
murzin66 <mikhailmurzinin@gmail.com>
2024-10-02 01:16:38 +0300

    remove MainPageItfo.tsx

--- project/Components/App.tsx
+++ project/Components/App.tsx
@@ -2,9 +2,6 @@ import MainPage from './MainPage.tsx';
 import {mainPageTypes} from '../../src/index.tsx';
 
 
-
-
-
 function App({ MainPageCardProps }: { MainPageCardProps: mainPageTypes[] }): JSX.Element {
 
     return(
